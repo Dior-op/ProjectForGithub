23f836702e55f1c1fa11d54714d6227dc56dcf98
Dior <Dior@gmial.com>
2026-01-06 17:51:59 +0500

    Initial Commit

---
+++ ProjectForGithub/ProjectForGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectForGithubApp.swift
+//  ProjectForGithub
+//
+//  Created by Shahriyor on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectForGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

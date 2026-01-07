edeff52d4c0f97e1e6b130d3c19f9eb211fe603f
Dior <Dior@gmial.com>
2026-01-07 18:49:57 +0500

    new version 1.2

--- ProjectForGithub/ContentView.swift
+++ ProjectForGithub/ContentView.swift
@@ -9,21 +9,65 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var text: String = "chain"
-    @State var textTwo: String = "main histore"
+    @StateObject var vm = MainScreenViewModel()
+    
+    @State var textTwo: String = "main.history.txt"
 
     var body: some View {
         VStack {
-            Image(systemName: "heart")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text(textTwo)
-            Text(text)
+            Text(vm.text)
+            TextField("napishite", text: $vm.text)
+                .textFieldStyle(.roundedBorder)
+                .padding()
+            
+            Button {
+                guard vm.text.count > 5 else { return }
+                vm.download(vm.text, textName: textTwo)
+            } label: {
+                Text("Soxranit")
+            }
+            
+            Button {
+                vm.text = vm.read(textTwo)
+            } label: {
+                Text("prochitat")
+            }
+
+            
+            VStack {
+                Image(systemName: "heart")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+class MainScreenViewModel: ObservableObject {
+    
+    @Published var text: String = ""
+
+    func download(_ text: String, textName: String) {
+        
+        guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+
+        let url = directory.appendingPathComponent(textName)
+        
+        do {
+            try text.write(to: url, atomically: true, encoding: .utf8)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func read(_ textName: String) -> String {
+        guard let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return ""}
+        let url = directory.appendingPathComponent(textName)
+        return (try? String(contentsOf: url, encoding: .utf8)) ?? ""
+    }
 }
